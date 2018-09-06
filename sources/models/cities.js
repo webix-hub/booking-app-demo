461ec328cfbb4fe33487eb201a8a6f8e0de1b29d
Nastya <akovalenko@xbsoftware.com>
2018-09-06 12:37:39 +0300

    [fix] better arrangement of city options

--- sources/models/cities.js
+++ sources/models/cities.js
@@ -3,21 +3,20 @@ export function getCities(){
 }
 
 const cities = [
-	{ id:1, value:"Berlin" },
-	{ id:2, value:"Kiev" },
-	{ id:3, value:"Minsk" },
-	{ id:4, value:"Moscow" },
-	{ id:5, value:"Prague" },
-	{ id:6, value:"Riga" },
-	{ id:7, value:"St.Petersburg" },
-	{ id:8, value:"Tallin" },
-	{ id:9, value:"Vilnius" },
-	{ id:10, value:"Warsaw" },
-	{ id:11, value:"Rome" },
-	{ id:12, value:"Oslo" },
-	{ id:13, value:"Barcelona" },
-	{ id:14, value:"Milan" },
-	{ id:15, value:"Zhulhany" },
-	{ id:16, value:"Stockholm" },
-	{ id:17, value:"Frankfurt" }
+	{ id:1, value:"Riga" },
+	{ id:2, value:"Warsaw" },
+	{ id:3, value:"Berlin" },
+	{ id:4, value:"Tallin" },
+	{ id:5, value:"Moscow" },
+	{ id:6, value:"Prague" },
+	{ id:7, value:"Vilnius" },
+	{ id:8, value:"Rome" },
+	{ id:9, value:"Barcelona" },
+	{ id:10, value:"Milan" },
+	{ id:11, value:"Kiev" },
+	{ id:12, value:"St.Petersburg" },
+	{ id:13, value:"Zhulhany" },
+	{ id:14, value:"Oslo" },
+	{ id:15, value:"Stockholm" },
+	{ id:16, value:"Frankfurt" }
 ];
