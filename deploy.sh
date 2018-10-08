fb9f24263547b2c46cc988f0f4e16013977aa712
Maksim Kozhukh <mkozhukh@ya.ru>
2018-10-08 13:32:15 +0300

    [dev] include data folder into the final package

--- deploy.sh
+++ deploy.sh
@@ -2,7 +2,7 @@
 set -e
 
 PACKAGE=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 32 | head -n 1).zip
-zip -rq ${PACKAGE} index.html ./codebase 
+zip -rq ${PACKAGE} index.html ./data ./codebase
 
 aws s3 cp --quiet ./${PACKAGE} s3://webix-temp/${PACKAGE}
 curl -s "http://webix.io:2280/h00ks/deploy-preview?token=${PREVIEW_HOOK_TOKEN}&file=${PACKAGE}&source=webix&component=bookin-app-demo&branch=${DRONE_BRANCH}"
